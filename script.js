fc73646f0c56aa26f20f0c0a3bffa312cf7dcbe7
Abdul Nur <abduljabar@Abduls-MacBook-Pro.local>
2022-11-10 17:27:48 -0600

    made more changes to java

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var capLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCasLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var capLetter = [];
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var specialCharacters = ["!","@","#","$","%","&","*","?"];
-var passworCreated = []
+for (var i=0; i < lowerCasLetter.length; i++){
+  capLetter[i] = lowerCasLetter[i].toUpperCase()
+}
+console.log(capLetter)
 function getRandom(list) {
   return list[ranpoint(0, list.length -1)]
 }
@@ -14,28 +17,35 @@ function ranpoint(min,max){
 }
 
 function generatePassword(){
-  var characters = window.prompt("how many characters")
+  var passworCreated = []
+  var characters = window.prompt("How long would you like to make your password?")
  var characterAnswer = characters
+ if (characterAnswer >= 129 || characterAnswer <= 7){
+  window.alert("Choose password length between 8-128")
+  return;
+ }
   console.log(characterAnswer);
 
-var number = window.confirm("include numbers?");
-var capital = window.confirm("do you want to include capital letters?");
-var special = window.confirm("include special characters?");
-var lowerCaseLet = window.confirm("do you want lower case?");
+var number = window.confirm("Would you like to include Numbers?");
+var capital = window.confirm("Would you like to include Capital Letters?");
+var special = window.confirm("Would you like to include special characters?");
+var lowerCaseLet = window.confirm("Would you like to include Lowercase Leters?");
 if(number === true){
   passworCreated.push(numbers)
 }
-  
+  if(lowerCaseLet === true){
+  passworCreated.push(lowerCasLetter)
+}
 if(capital === true){
   passworCreated.push(capLetter)
 }
-
 if(special === true){
   passworCreated.push(specialCharacters)
 }
-if(lowerCaseLet === true){
-  passworCreated.push(lowerCase)
+if(lowerCaseLet && capital === true){
+  passworCreated.push(lowerCasLetter,capLetter)
 }
+
 console.log(passworCreated);
 var createdPasscode = "";
 for (var i = 0;i < characterAnswer; i++){
